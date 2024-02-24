5a056b5e177201fb7035553de9f45d0878b92e02
jsalgueroibarrola <82667763+jsalgueroibarrola@users.noreply.github.com>
2024-02-24 08:58:36 +0100

    Add methods for fetching chart data of threads

--- backend/src/main/java/es/codeurjc/backend/repository/ThreadRepository.java
+++ backend/src/main/java/es/codeurjc/backend/repository/ThreadRepository.java
@@ -1,14 +1,25 @@
 package es.codeurjc.backend.repository;
 
 import java.util.Optional;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import es.codeurjc.backend.model.Thread;
 import es.codeurjc.backend.model.User;
-
 
 public interface ThreadRepository extends JpaRepository<Thread, Long> {
     Optional<List<Thread>> findByOwner(User owner);
 
     Optional<Thread> findByName(String name);
+
+    @Query("SELECT COUNT(t) FROM Thread t WHERE t.owner = :owner AND DATE(t.createdAt) = :date")
+    Long countByCreatedAt(User owner, LocalDate date);
+
+    @Query("SELECT COUNT(t) FROM Thread t WHERE t.owner = :owner AND FUNCTION('MONTH', t.createdAt) = :month AND FUNCTION('YEAR', t.createdAt) = :year")
+    Long countByMonthAndYear(@Param("owner") User owner, @Param("month") int month, @Param("year") int year);
+
+    @Query("SELECT COUNT(t) FROM Thread t WHERE t.owner = :owner AND FUNCTION('YEAR', t.createdAt) = :year")
+    Long countByYear(@Param("owner") User owner, @Param("year") int year);
 }
--- backend/src/main/java/es/codeurjc/backend/service/ThreadService.java
+++ backend/src/main/java/es/codeurjc/backend/service/ThreadService.java
@@ -1,7 +1,7 @@
 package es.codeurjc.backend.service;
 
+import java.time.LocalDate;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import es.codeurjc.backend.model.User;
@@ -10,11 +10,23 @@ import es.codeurjc.backend.repository.ThreadRepository;
 
 @Service
 public class ThreadService {
-    
+
     @Autowired
     private ThreadRepository threadRepository;
 
-    public List<Thread> getThreadsByOwner(User owner){
+    public List<Thread> getThreadsByOwner(User owner) {
         return threadRepository.findByOwner(owner).orElseThrow();
+    }
+
+    public Long getTotalThreadsForDay(User owner, LocalDate date) {
+        return threadRepository.countByCreatedAt(owner, date);
+    }
+
+    public Long getTotalThreadsForMonth(User owner, int month, int year) {
+        return threadRepository.countByMonthAndYear(owner, month, year);
+    }
+
+    public Long getTotalThreadsForYear(User owner, int year) {
+        return threadRepository.countByYear(owner, year);
     }
 }
