57d9f42842d9fe141ffee274c6976a6cea6c80df
miguel-quero <m.quero.2021@alumnos.urjc.es>
2024-02-25 18:41:11 +0100

    view forums

--- backend/src/main/java/es/codeurjc/backend/controller/HomeController.java
+++ backend/src/main/java/es/codeurjc/backend/controller/HomeController.java
@@ -2,16 +2,20 @@ package es.codeurjc.backend.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import es.codeurjc.backend.service.ForumService;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ForumService forumService;
 
     @ModelAttribute
     public void addAttributes(Model model, HttpServletRequest request) {
@@ -27,6 +31,8 @@ public class HomeController {
 		} else {
 			model.addAttribute("logged", false);
 		}
+        model.addAttribute("forums",forumService.getAllForums());
+        
 	}
 
 
