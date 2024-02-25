6483f5bad428bc91914e61dcb467bcf48f1ca27b
jsalgueroibarrola <82667763+jsalgueroibarrola@users.noreply.github.com>
2024-02-25 19:45:11 +0100

    add methods for fetching chart data of posts

--- backend/src/main/java/es/codeurjc/backend/repository/PostRepository.java
+++ backend/src/main/java/es/codeurjc/backend/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package es.codeurjc.backend.repository;
 
 import java.util.Optional;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -19,4 +20,13 @@ public interface PostRepository extends JpaRepository<Post, Long> {
   @Modifying
   @Query("UPDATE Post p SET p.owner.id = :newOwnerId WHERE p.owner.id = :currentOwnerId")
   void changeOwnerOfPosts(@Param("currentOwnerId") Long currentOwnerId, @Param("newOwnerId") Long newOwnerId);
+
+  @Query("SELECT COUNT(t) FROM Post t WHERE t.owner = :owner AND DATE(t.createdAt) = :date")
+  Long countByCreatedAt(User owner, LocalDate date);
+
+  @Query("SELECT COUNT(t) FROM Post t WHERE t.owner = :owner AND FUNCTION('MONTH', t.createdAt) = :month AND FUNCTION('YEAR', t.createdAt) = :year")
+  Long countByMonthAndYear(@Param("owner") User owner, @Param("month") int month, @Param("year") int year);
+
+  @Query("SELECT COUNT(t) FROM Post t WHERE t.owner = :owner AND FUNCTION('YEAR', t.createdAt) = :year")
+  Long countByYear(@Param("owner") User owner, @Param("year") int year);
 }
--- backend/src/main/java/es/codeurjc/backend/service/PostService.java
+++ backend/src/main/java/es/codeurjc/backend/service/PostService.java
@@ -1,5 +1,6 @@
 package es.codeurjc.backend.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,23 @@ import es.codeurjc.backend.repository.PostRepository;
 
 @Service
 public class PostService {
-    
+
     @Autowired
     private PostRepository postRepository;
 
-    public List<Post> getPostByOwner(User owner){
+    public List<Post> getPostByOwner(User owner) {
         return postRepository.findByOwner(owner).orElseThrow();
+    }
+
+    public Long getTotalPostsForDay(User owner, LocalDate date) {
+        return postRepository.countByCreatedAt(owner, date);
+    }
+
+    public Long getTotalPostsForMonth(User owner, int month, int year) {
+        return postRepository.countByMonthAndYear(owner, month, year);
+    }
+
+    public Long getTotalPostsForYear(User owner, int year) {
+        return postRepository.countByYear(owner, year);
     }
 }
