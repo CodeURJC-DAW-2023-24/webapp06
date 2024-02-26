eb5727cd994bfe23357092f2debc2ec27f302503
miguel-quero <m.quero.2021@alumnos.urjc.es>
2024-02-26 12:43:25 +0100

    Register user

--- backend/src/main/java/es/codeurjc/backend/controller/registerController.java
+++ backend/src/main/java/es/codeurjc/backend/controller/registerController.java
@@ -2,13 +2,21 @@ package es.codeurjc.backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
-public class registerController {
+public class RegisterController {
 
   @GetMapping("/register")
   public String register() {
     return "register";
+  }
+  
+  @PostMapping("/registerUser")
+  public String register(@RequestBody String username, @RequestBody String password, @RequestBody String email) {
+      return "register_done";
   }
   
 }
