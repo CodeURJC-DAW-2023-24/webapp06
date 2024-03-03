5618cf656629f369a96b2b2fe2c57b33cbad4999
AdriDM-urjc <a.duenas.2021@alumnos.urjc.es>
2024-03-03 19:31:01 +0100

    Report screen set up

---
+++ backend/src/main/java/es/codeurjc/backend/controller/ReportController.java
@@ -0,0 +1,34 @@
+package es.codeurjc.backend.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class ReportController {
+
+    @ModelAttribute
+	public void addAttributes(Model model, HttpServletRequest request) {
+
+		Principal principal = request.getUserPrincipal();
+
+		if (principal != null) {
+			model.addAttribute("logged", true);
+			model.addAttribute("username", principal.getName());
+			model.addAttribute("admin", request.isUserInRole("ADMIN"));
+
+		} else {
+			model.addAttribute("logged", false);
+		}
+	}
+
+    @GetMapping("/reports")
+    public String getChart() {
+        return "reports";
+    }
+}
