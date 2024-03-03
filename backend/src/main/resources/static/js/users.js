b27007aa236f00c22c78d9cd1ba609c7b0c3f3dc
jsalgueroibarrola <82667763+jsalgueroibarrola@users.noreply.github.com>
2024-03-03 09:04:33 +0100

    Add pagination to users template

---
+++ backend/src/main/resources/static/js/users.js
@@ -0,0 +1,96 @@
+var page = 0;
+
+document.addEventListener("DOMContentLoaded", function () {
+  addNewElements();
+});
+
+function createUserHtml(username) {
+  return `
+    <div class="list-group-item d-flex align-items-center justify-content-between">
+      <div class="d-flex align-items-center">
+        <img src="/image/user/${username}" class="rounded-circle" style="width: 50px; height: 50px" alt="User Photo" />
+        <strong class="ml-3">${username}</strong>
+      </div>
+      <div>
+        <div>
+          <a class="btn btn-link text-primary text-decoration-none p-0 mr-2" title="view profile" href="/user/profile/${username}">
+            <i class="fas fa-eye"></i>
+          </a>
+          <a class="btn btn-link text-primary text-decoration-none p-0 mr-2" title="edit profile" href="/user/edit-profile?username=${username}">
+            <i class="fas fa-edit"></i>
+          </a>
+          <button type="button" class="btn btn-link text-decoration-none btn-delete p-0 mr-2" data-toggle="modal" data-target="#deleteAccountModal${username}">
+            <i class="fas fa-trash-alt btn-delete"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="deleteAccountModal${username}" tabindex="-1" aria-labelledby="modalLabel${username}" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="modalLabel${username}">Confirm Deletion</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            Are you sure you want to delete your account? This action cannot be undone.
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            <a type="button" class="btn btn-danger btn-confirm-delete" href="/user/delete/${username}">Delete Account</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+async function addNewElements() {
+  showSpinner(true);
+
+  const response = await fetch(
+    "https://localhost:8443/user/paginated?page=" + page + "&size=10",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const data = await response.json();
+
+  page += 1;
+
+  if (page >= data.totalPages) {
+    document.getElementById("loadMoreUsersButton").style.display = "none";
+  }
+
+  const container = document.getElementById("user-container");
+  if (!container) {
+    console.error("The container was not found in the DOM");
+    return;
+  }
+
+  showSpinner(false);
+
+  data.content.forEach((user) => {
+    container.insertAdjacentHTML("beforeend", createUserHtml(user.username));
+  });
+}
+
+function showSpinner(show) {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = show ? "block" : "none";
+}
+
+function searchUser() {
+  const username = document.getElementById("usernameInput").value;
+  console.log("Searching for user:", username);
+}
