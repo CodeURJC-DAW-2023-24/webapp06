031c0f8994491903f820f63886e8df33b8ff2545
JorgeCarbonero <145460112+JorgeCarbonero@users.noreply.github.com>
2024-03-03 15:12:44 +0100

    Liking and disliking are mutually independent

--- backend/src/main/resources/static/js/likes.js
+++ backend/src/main/resources/static/js/likes.js
@@ -2,7 +2,12 @@ async function toggleLike(likesButton) {
   let likeIcon = likesButton.querySelector("#likeIcon");
   let likeText = likesButton.querySelector("#likeText");
 
+  let dislikesButton = likesButton.parentNode.querySelector("#dislikes");
+  let dislikeIcon = dislikesButton.querySelector("#dislikeIcon");
+  let dislikeText = dislikesButton.querySelector("#dislikeText");
+
   let isLiked = likesButton.getAttribute("data-liked") === "true";
+  let isDisliked = dislikesButton.getAttribute("data-disliked") === "true";
 
   if (isLiked) {
     likesButton.setAttribute("data-liked", "false");
@@ -12,6 +17,11 @@ async function toggleLike(likesButton) {
     likesButton.setAttribute("data-liked", "true");
     likeIcon.classList.add("liked"); // Add the 'liked' class
     likeText.textContent = parseInt(likeText.textContent) + 1; // Increase likes count
+    if (isDisliked) {
+      dislikesButton.setAttribute("data-disliked", "false");
+      dislikeIcon.classList.remove("disliked"); // Remove the 'liked' class
+      dislikeText.textContent = parseInt(dislikeText.textContent) - 1; // Decrease likes count
+    }
   }
 }
 
@@ -19,7 +29,12 @@ async function toggleDislike(dislikesButton) {
   let dislikeIcon = dislikesButton.querySelector("#dislikeIcon");
   let dislikeText = dislikesButton.querySelector("#dislikeText");
 
+  let likesButton = dislikesButton.parentNode.querySelector("#likes");
+  let likeIcon = likesButton.querySelector("#likeIcon");
+  let likeText = likesButton.querySelector("#likeText");
+
   let isDisliked = dislikesButton.getAttribute("data-disliked") === "true";
+  let isLiked = likesButton.getAttribute("data-liked") === "true";
 
   if (isDisliked) {
     dislikesButton.setAttribute("data-disliked", "false");
@@ -29,5 +44,10 @@ async function toggleDislike(dislikesButton) {
     dislikesButton.setAttribute("data-disliked", "true");
     dislikeIcon.classList.add("disliked"); // Add the 'liked' class
     dislikeText.textContent = parseInt(dislikeText.textContent) + 1; // Increase likes count
+    if (isLiked) {
+      likesButton.setAttribute("data-liked", "false");
+      likeIcon.classList.remove("liked"); // Remove the 'liked' class
+      likeText.textContent = parseInt(likeText.textContent) - 1; // Decrease likes count
+    }
   }
 }
