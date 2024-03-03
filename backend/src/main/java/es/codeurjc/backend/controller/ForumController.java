f0cac7f0cbf28648cde36e7e31b7dd0d21d24010
JorgeCarbonero <145460112+JorgeCarbonero@users.noreply.github.com>
2024-03-03 20:10:03 +0100

    Threads can be added to forums

--- backend/src/main/java/es/codeurjc/backend/controller/ForumController.java
+++ backend/src/main/java/es/codeurjc/backend/controller/ForumController.java
@@ -1,6 +1,7 @@
 package es.codeurjc.backend.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +13,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import es.codeurjc.backend.model.Forum;
+import es.codeurjc.backend.model.Post;
 import es.codeurjc.backend.model.Thread;
+import es.codeurjc.backend.model.User;
 import es.codeurjc.backend.service.ForumService;
 import es.codeurjc.backend.service.ThreadService;
+import es.codeurjc.backend.service.UserService;
 
 @Controller
 @RequestMapping("/f")
 public class ForumController {
+    
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private ThreadService threadService;
@@ -56,5 +65,16 @@ public class ForumController {
         model.addAttribute("forums", forumService.getAllForums());
         
         return "forum";
+    }
+
+    @PostMapping("/{forumName}/addThread")
+    public String addPost(Model model, Principal principal, @RequestParam("post-form-text") String threadName,
+            @PathVariable String forumName) throws Exception {
+        String name = principal.getName();
+        User activeUser = userService.getUserByUsername(name);
+        Forum forum = forumService.getForumByName(forumName);
+        Thread thread = new Thread(threadName, forum, new ArrayList<Post>(), activeUser);
+        forumService.addThreadToForum(forum, thread);
+        return "redirect:/t/" + threadName;
     }
 }
--- backend/src/main/java/es/codeurjc/backend/service/ForumService.java
+++ backend/src/main/java/es/codeurjc/backend/service/ForumService.java
@@ -11,15 +11,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.codeurjc.backend.model.Forum;
-
+import es.codeurjc.backend.model.Post;
 import es.codeurjc.backend.model.Thread;
 import es.codeurjc.backend.repository.ForumRepository;
+import es.codeurjc.backend.repository.ThreadRepository;
 
 @Service
 public class ForumService {
     @Autowired
     private ForumRepository forumRepository;
-    
+
+    @Autowired
+    private ThreadRepository threadRepository;
+
     public List<Forum> getAllForums() {
         return forumRepository.findAll();
     }
@@ -35,16 +39,22 @@ public class ForumService {
 
     public List<Forum> getTrendingForums(boolean logged) {
         List<Forum> forums = new ArrayList<>();
-        if (logged){
-            
+        if (logged) {
+
         } else {
-            List<Long>forumsid = forumRepository.findForumsWithMostPostsLast48Hours();
-            if(!forumsid.isEmpty()){
+            List<Long> forumsid = forumRepository.findForumsWithMostPostsLast48Hours();
+            if (!forumsid.isEmpty()) {
                 forums = forumRepository.findAllById(forumsid);
             }
         }
-        
+
         return forums;
+    }
+
+    public void addThreadToForum(Forum forum, Thread thread) {
+        threadRepository.save(thread);
+        forum.getThreads().add(thread);
+        forumRepository.save(forum);
     }
 
 }
