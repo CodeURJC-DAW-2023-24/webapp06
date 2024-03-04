ac330a5653dfe786763e4f8bccaaa0ebf75f8a2c
JorgeCarbonero <145460112+JorgeCarbonero@users.noreply.github.com>
2024-03-04 04:13:54 +0100

    Prepared DataLoader for presentation

--- backend/src/main/java/es/codeurjc/backend/loader/DataLoader.java
+++ backend/src/main/java/es/codeurjc/backend/loader/DataLoader.java
@@ -32,9 +32,9 @@ public class DataLoader {
         public void run() throws Exception {
                 Resource imageUser1 = new ClassPathResource("example/user/user1.png");
                 Blob blobImageUser1 = BlobProxy.generateProxy(imageUser1.getInputStream(), imageUser1.contentLength());
-                User user1 = new User("username1", "user1@example.com", passwordEncoder.encode("password1"), "USER");
+                User user1 = new User("User", "user1@example.com", passwordEncoder.encode("password1"), "USER");
                 user1.setImageFile(blobImageUser1);
-                User user2 = new User("username2", "user2@example.com", passwordEncoder.encode("password2"), "USER",
+                User user2 = new User("Admin", "user2@example.com", passwordEncoder.encode("password2"), "USER",
                                 "ADMIN");
                 User user3 = new User("CyberKnight", "cyberknight@example.com", passwordEncoder.encode("securePass3"),
                                 "USER");
@@ -79,31 +79,35 @@ public class DataLoader {
                 Forum forum8 = new Forum("Travel", null, "plane");
                 Forum forum9 = new Forum("Gaming", null, "gamepad");
 
-                Thread thread1 = new Thread("Saludos", forum1, new ArrayList<Post>(), user1);
-                Thread thread2 = new Thread("Pruebas", forum2, new ArrayList<Post>(), user2);
+                Thread thread1 = new Thread("Discussion What\'s the best Brandon Sanderson Book", forum1,
+                                new ArrayList<Post>(), user1);
+                Thread thread2 = new Thread("The new Elden Ring DLC looks great", forum2, new ArrayList<Post>(), user2);
+                Thread thread3 = new Thread("BOOK1", forum1, new ArrayList<Post>(), user2);
+                Thread thread4 = new Thread("BOOK2", forum1, new ArrayList<Post>(), user2);
+                Thread thread5 = new Thread("BOOK3", forum1, new ArrayList<Post>(), user2);
+                Thread thread6 = new Thread("BOOK4", forum1, new ArrayList<Post>(), user2);
+                Thread thread7 = new Thread("BOOK5", forum1, new ArrayList<Post>(), user2);
+                Thread thread8 = new Thread("BOOK6", forum1, new ArrayList<Post>(), user2);
+                Thread thread9 = new Thread("BOOK7", forum1, new ArrayList<Post>(), user2);
+                Thread thread10 = new Thread("BOOK8", forum1, new ArrayList<Post>(), user2);
+                Thread thread11 = new Thread("BOOK9", forum1, new ArrayList<Post>(), user2);
+                Thread thread12 = new Thread("BOOK10", forum1, new ArrayList<Post>(), user2);
+                Thread thread13 = new Thread("BOOK11", forum1, new ArrayList<Post>(), user2);
 
                 Resource imagePost1 = new ClassPathResource("example/post/foto_post.jpg");
                 Blob blobImagePost1 = BlobProxy.generateProxy(imagePost1.getInputStream(), imagePost1.contentLength());
 
-                Post post1 = new Post("hola", blobImagePost1, user1, thread1, userLikes, userDislikes, 1);
-                Post post2 = new Post("adios", null, user2, thread1, userLikes, userDislikes, 3);
-                Post post3 = new Post(
-                                "In the quiet town of Neo-Silica, a revolution brews under the neon lights.", null,
-                                user1, thread1, userLikes, userDislikes, 1);
-
-                Post post4 = new Post(
-                                "Far beyond the reach of civilization, the ancient forest of Eldergrove whispers secrets of a bygone era.",
-                                null, user2, thread1, userLikes, userDislikes, 2);
-
-                Post post5 = new Post(
-                                "Galactic Odyssey: A Journey Beyond Stars", null, user3, thread2, userLikes,
+                Post post1 = new Post("This is it, convince otherwise", blobImagePost1, user1, thread1, userLikes,
+                                userDislikes, 1);
+                Post post2 = new Post("IMO the Mistborn series is better", null, user2, thread1, userLikes,
                                 userDislikes, 3);
-
-                Post post6 = new Post(
-                                "The Last Codebender", null, user4, thread2, userLikes, userDislikes, 4);
-
-                Post post7 = new Post(
-                                "Whispers in the Digital Wind", null, user5, thread2, userLikes, userDislikes, 5);
+                Post post3 = new Post("Are u out of your mind?", null, user1, thread1, userLikes, userDislikes, 1);
+                Post post4 = new Post("It\'s a lot better, say otherwise and u r WRONG!!!", null, user2, thread1,
+                                userLikes, userDislikes, 2);
+                Post post5 = new Post("FINALLY!!!!", null, user3, thread2, userLikes,
+                                userDislikes, 3);
+                Post post6 = new Post("I couldn\'t believe it!", null, user4, thread2, userLikes, userDislikes, 4);
+                Post post7 = new Post("It will be great", null, user5, thread2, userLikes, userDislikes, 5);
 
                 // postRepository.saveAll(Arrays.asList(post1, post2));
                 //
@@ -113,7 +117,7 @@ public class DataLoader {
                 thread1.setPosts(Arrays.asList(post1, post2, post3, post4));
                 thread2.setPosts(Arrays.asList(post5, post6, post7));
 
-                forum1.setThreads(Arrays.asList(thread1));
+                forum1.setThreads(Arrays.asList(thread1, thread3, thread4, thread5, thread6, thread7, thread8, thread9, thread10, thread11, thread12, thread13));
                 forum2.setThreads(Arrays.asList(thread2));
                 forumRepository.save(forum1);
                 forumRepository.save(forum2);
