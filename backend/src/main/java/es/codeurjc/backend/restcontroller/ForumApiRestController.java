a54e4910379f9ac55b39e6bbab88396550a51d81
JorgeCarbonero <jorge.carbasin@gmail.com>
2024-04-01 23:06:36 +0200

    Added getThreadById and changed getThreadByForum

--- backend/src/main/java/es/codeurjc/backend/restcontroller/ForumApiRestController.java
+++ backend/src/main/java/es/codeurjc/backend/restcontroller/ForumApiRestController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
 @RestController
 @RequestMapping("/api/forums")
 public class ForumApiRestController {
@@ -30,30 +28,35 @@ public class ForumApiRestController {
   private ForumService forumService;
 
   final private String ERROR_OCURRED = "Error occurred, try again later";
-  
-  @GetMapping
-  @Operation(summary = "Gets all forums", description = "Provides a list of all the forums in the web.", responses = {@ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json")), @ApiResponse(responseCode = "500", description = "Internal server error")
-})
-public ResponseEntity<?> getForums() {
-  try {
-    List<Forum> forums = forumService.getAllForumsExcp();
-    return ResponseEntity.ok(forums);
-  } catch (JsonProcessingException exception) {
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ERROR_OCURRED);
-  }
-}
 
-@GetMapping("/{id}")
-@Operation(summary = "Gets a forum by id", description = "Provides a forum based on the specified id.", responses = {@ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json")), @ApiResponse(responseCode = "404", description = "Forum not found"), @ApiResponse(responseCode = "500", description = "Internal server error")
-})
-public ResponseEntity<?> getForumById(@PathVariable Long id) {
-        try {
-            Forum forum = forumService.getForumById(id);
-            return ResponseEntity.ok(forum);
-        } catch (ForumNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Forum not found");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ERROR_OCURRED);
-        }
+  @GetMapping
+  @Operation(summary = "Gets all forums", description = "Provides a list of all the forums in the web.", responses = {
+      @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json")),
+      @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
+  public ResponseEntity<?> getForums() {
+    try {
+      List<Forum> forums = forumService.getAllForumsExcp();
+      return ResponseEntity.ok(forums);
+    } catch (JsonProcessingException exception) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ERROR_OCURRED);
     }
+  }
+
+  @GetMapping("/{id}")
+  @Operation(summary = "Gets a forum by id", description = "Provides a forum based on the specified id.", responses = {
+      @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json")),
+      @ApiResponse(responseCode = "404", description = "Forum not found"),
+      @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
+  public ResponseEntity<?> getForumById(@PathVariable Long id) {
+    try {
+      Forum forum = forumService.getForumById(id);
+      return ResponseEntity.ok(forum);
+    } catch (ForumNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Forum not found");
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ERROR_OCURRED);
+    }
+  }
 }
