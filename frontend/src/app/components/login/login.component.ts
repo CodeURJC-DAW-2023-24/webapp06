defe0b9245a2dfcc471e0c47b9b342e5e7fb8ca2
jsalgueroibarrola <82667763+jsalgueroibarrola@users.noreply.github.com>
2024-04-09 12:06:14 +0200

    Partial implementation of login system

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(public loginService: LoginService) {}
 
+  logIn(event: any, user: string, pass: string) {
+    event.preventDefault();
+    console.log('prueba');
+    this.loginService.logIn(user, pass);
+  }
+
+  prueba(): any {
+    console.log('prueba');
+    alert('Prueba');
+  }
 }
--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environments';
+
+const BASE_URL = '/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
+  user?: User;
+  logged: boolean = false;
+  private apiUrl = environment.backendUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    //this.reqIsLogged();
+  }
+
+  reqIsLogged() {
+    this.http.get('/api/users/me', { withCredentials: true }).subscribe(
+      (response) => {
+        this.user = response as User;
+        this.logged = true;
+      },
+      (error) => {
+        if (error.status != 404) {
+          console.error(
+            'Error when asking if logged: ' + JSON.stringify(error)
+          );
+        }
+      }
+    );
+  }
+
+  logIn(user: string, pass: string) {
+    console.log('respuesta aaa');
+    this.http
+      .post(
+        this.apiUrl + BASE_URL + '/login',
+        { username: user, password: pass },
+        { withCredentials: true }
+      )
+      .subscribe(
+        (response) => console.log('respuesta', response),
+        (error) => alert('Wrong credentials')
+      );
+  }
+
+  logOut() {
+    return this.http
+      .post(this.apiUrl + BASE_URL + '/logout', { withCredentials: true })
+      .subscribe((resp: any) => {
+        console.log('LOGOUT: Successfully');
+        this.logged = false;
+        this.user = undefined;
+      });
+  }
+
+  isLogged(): boolean {
+    return this.logged;
+  }
+
+  isAdmin() {
+    return this.user && this.user.roles.indexOf('ADMIN') !== -1;
+  }
+
+  currentUser() {
+    return this.user;
+  }
 }
