b108fbaf7ffb91447ca356dbc17abfd1dc5ef4c8
miguel-quero <m.quero.2021@alumnos.urjc.es>
2024-04-15 12:31:21 +0200

    UserApiRestController PUT done

--- backend/src/main/java/es/codeurjc/backend/restcontroller/UserApiRestController.java
+++ backend/src/main/java/es/codeurjc/backend/restcontroller/UserApiRestController.java
@@ -1,10 +1,14 @@
 package es.codeurjc.backend.restcontroller;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+
+import javax.sql.rowset.serial.SerialBlob;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,8 +16,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.mysql.cj.jdbc.Blob;
 
 import es.codeurjc.backend.dto.UserDTO;
 import es.codeurjc.backend.model.User;
@@ -31,6 +40,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @RestController
 @RequestMapping("/api/users")
@@ -179,5 +189,101 @@ public class UserApiRestController {
 
         }
     }
+
+    @PutMapping("/{userId}")
+    @Operation(summary = "Edit user", description = "Edit user information.")
+    public ResponseEntity<?> editUser(@AuthenticationPrincipal UserDetails userDetails,
+                                      @PathVariable Long userId,
+                                      @RequestBody UserDTO updatedUserInfo) {
+        if (userDetails != null) {
+            try {
+                
+                Optional<User> existingUserOptional = userService.getUserById(userId);
+                if (!existingUserOptional.isPresent()) {
+                    return new ResponseEntity<>("User with id " + userId + " not found.", HttpStatus.NOT_FOUND);
+                }
+    
+                User existingUser = existingUserOptional.get();
+                
+                
+                if (existingUser.getUsername().equals(userDetails.getUsername())
+                        || userService.isAdmin(userDetails.getUsername())) {
+                    
+                    
+                    existingUser.setEmail(updatedUserInfo.getEmail());
+                    
+                    
+                    userService.save(existingUser);
+    
+                    return new ResponseEntity<>("User with id " + userId + " successfully updated.", HttpStatus.OK);
+                } else {
+                   
+                    return new ResponseEntity<>("User with id " + userId + " can only be edited by its owner or an administrator.", HttpStatus.UNAUTHORIZED);
+                }
+            } catch (Exception e) {
+                
+                return new ResponseEntity<>("An error occurred while updating user with id " + userId + ".", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            
+            return new ResponseEntity<>("Authenticated user not found.", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PutMapping("/{userId}/image")
+    @Operation(summary = "Edit user image", description = "Edit user image by id.", responses = {
+            @ApiResponse(responseCode = "200", description = "User image edited successfully"),
+            @ApiResponse(responseCode = "400", description = "Bad request body"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<?> editUserImage(@AuthenticationPrincipal UserDetails userDetails,
+                                        @PathVariable Long userId,
+                                        @RequestPart MultipartFile image) {
+        if (userDetails != null) {
+            try {
+                
+                Optional<User> existingUserOptional = userService.getUserById(userId);
+                if (!existingUserOptional.isPresent()) {
+                    return new ResponseEntity<>("User with id " + userId + " not found.", HttpStatus.NOT_FOUND);
+                }
+
+                User existingUser = existingUserOptional.get();
+
+            
+                if (existingUser.getUsername().equals(userDetails.getUsername())
+                        || userService.isAdmin(userDetails.getUsername())) {
+
+                    if (image == null) {
+                        return new ResponseEntity<>("Include an image.", HttpStatus.BAD_REQUEST);
+                    }
+
+                    try {
+                        
+                        existingUser.setImageFile(new SerialBlob(image.getBytes()));
+                        userService.save(existingUser);
+
+                        return new ResponseEntity<>("User image edited successfully.", HttpStatus.OK);
+                    } catch (IOException | SQLException e) {
+                        return new ResponseEntity<>("Error updating user image.", HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                } else {
+                    
+                    return new ResponseEntity<>("User image can only be edited by its owner or an administrator.", HttpStatus.UNAUTHORIZED);
+                }
+            } catch (Exception e) {
+            
+                return new ResponseEntity<>("An error occurred while updating user image for user with id " + userId + ".", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            
+            return new ResponseEntity<>("Authenticated user not found.", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    
+
+
 
 }
