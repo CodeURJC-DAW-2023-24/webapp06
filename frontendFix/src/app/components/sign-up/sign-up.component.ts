615c855e23808dff25208dba9dae9385843a491f
miguel-quero <m.quero.2021@alumnos.urjc.es>
2024-04-30 11:06:22 +0200

    register almost done

--- frontendFix/src/app/components/sign-up/sign-up.component.ts
+++ frontendFix/src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+  @ViewChild('username') usernameInput: any;
+  @ViewChild('email') emailInput: any;
+  @ViewChild('password') passwordInput: any;
 
+  onSubmit() { 
+    const username: string = this.usernameInput.nativeElement.value;
+    const email: string = this.emailInput.nativeElement.value;
+    const password: string = this.passwordInput.nativeElement.value;
+    console.log(username);
+    console.log(email);
+    console.log(password);
+  }
 }
